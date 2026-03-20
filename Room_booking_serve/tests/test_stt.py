74510621acac2f3ad830403bf48c585d880085fb
Lensi Gondaliya <lensi1110@gmail.com>
2026-03-20 02:23:31 -0400

    Fix STT test file path

--- Room_booking_serve/tests/test_stt.py
+++ Room_booking_serve/tests/test_stt.py
@@ -1,14 +1,14 @@
 import sys
 from pathlib import Path
 
-# Allow importing from src/
 sys.path.append(str(Path(__file__).resolve().parents[1] / "src"))
 
 from stt import convert_wav_to_text
 
 
 def test_speech_to_text_returns_string():
-    result_text = convert_wav_to_text("data/voice4.wav")
+    audio_file = Path(__file__).resolve().parents[1] / "data" / "voice4.wav"
+    result_text = convert_wav_to_text(str(audio_file))
 
     assert isinstance(result_text, str)
     assert len(result_text) > 0
